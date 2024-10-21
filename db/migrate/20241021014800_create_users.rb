d9b03eaf5e1eef8076215842eb22d95b12211421
Ubuntu <2211140043v@ed.fuk.kindai.ac.jp>
2024-10-21 02:34:50 +0000

    test

--- db/migrate/20241021014800_create_users.rb
+++ db/migrate/20241021014800_create_users.rb
@@ -1,8 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
-    create_table :users do |t|
+    create_table :users, if_not_exists: true do |t|
       t.string :uid
       t.string :pass
+
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_21_015306) do
   create_table "users", force: :cascade do |t|
     t.string "uid"
     t.string "pass"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
